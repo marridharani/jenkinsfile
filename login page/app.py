d245c824a1315d5bf9c5f3500fe1a2cad3172cc2
marridharani <marridharani64@gmail.com>
2026-08-04 10:56:12 +0530

    Create app.py

---
+++ login page/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+
+app = Flask(_name_)
+
+USERNAME = "admin"
+PASSWORD = "admin123"
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.json
+
+    if data["username"] == USERNAME and data["password"] == PASSWORD:
+        return jsonify({"message": "Login Successful"})
+
+    return jsonify({"message": "Invalid Username or Password"}), 401
+
+if _name_ == "_main_":
+    app.run(debug=True)
